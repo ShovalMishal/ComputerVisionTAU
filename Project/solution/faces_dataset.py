8ff5e7e16cad767317482aef6da161276388c44f
Shoval Mishal <shovalmishal1212@gmail.com>
2022-12-26 18:24:47 +0200

    step1

---
+++ Project/solution/faces_dataset.py
@@ -0,0 +1,48 @@
+"""Custom faces dataset."""
+import os
+
+import torch
+from PIL import Image
+from torch.utils.data import Dataset
+import numpy as np
+
+
+class FacesDataset(Dataset):
+    """Faces dataset.
+
+    Attributes:
+        root_path: str. Directory path to the dataset. This path has to
+        contain a subdirectory of real images called 'real' and a subdirectory
+        of not-real images (fake / synthetic images) called 'fake'.
+        transform: torch.Transform. Transform or a bunch of transformed to be
+        applied on every image.
+    """
+    def __init__(self, root_path: str, transform=None):
+        """Initialize a faces dataset."""
+        self.root_path = root_path
+        self.real_image_names = os.listdir(os.path.join(self.root_path, 'real'))
+        self.fake_image_names = os.listdir(os.path.join(self.root_path, 'fake'))
+        self.transform = transform
+
+
+    def __getitem__(self, index) -> tuple[torch.Tensor, int]:
+        """Get a sample and label from the dataset."""
+        num_real = len(self.real_image_names)
+        if index < num_real:
+            label = 0
+            image_path = os.path.join(self.root_path, "real", self.real_image_names[index])
+        else:
+            label = 1
+            image_path = os.path.join(self.root_path, "fake", self.fake_image_names[index - num_real])
+
+        with Image.open(image_path) as im:
+            if self.transform:
+                image = self.transform(im)
+
+        return image, label
+
+    def __len__(self):
+        """Return the number of images in the dataset."""
+        num_real = len(self.real_image_names)
+        num_fake = len(self.fake_image_names)
+        return num_real + num_fake
